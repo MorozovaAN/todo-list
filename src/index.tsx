3fbaa057f20f28e22840368378db37e75b45806f
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-03-25 21:40:22 +0400

    fix: add hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./index.css";
 import { App } from "./app/App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "typeface-roboto";
+import "./index.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
